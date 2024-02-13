91a08366332edc0ec2e9f34334fa30d48cecb0ee
usachin555 <sachinsac805@gmail.com>
2024-02-14 01:15:29 +0530

    component table jsx

---
+++ src/components/functional components/table.js
@@ -0,0 +1,46 @@
+import Image from "./image.js"
+function Table(){
+  return(
+    <div>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Country</th>
+        <th>Image</th>
+      </tr>
+      <tr>
+        <td>Rohit</td>
+        <td>30</td>
+        <td>india</td>
+        <td><Image></Image></td>
+      </tr>
+      <tr>
+        <td>Rohit</td>
+        <td>30</td>
+        <td>india</td>
+        <td><Image></Image></td>
+      </tr>
+      <tr>
+        <td>Rohit</td>
+        <td>4 0</td>
+        <td>india</td>
+        <td><Image></Image></td>
+      </tr>
+      <tr>
+        <td>Rohit</td>
+        <td>30</td>
+        <td>india</td>
+        <td><Image></Image></td>
+      </tr>
+      <tr>
+        <td>Rohit</td>
+        <td>30</td>
+        <td>india</td>
+        <td><Image></Image></td>
+      </tr>
+    </table>
+    </div>
+  )
+}
+export default Table;
