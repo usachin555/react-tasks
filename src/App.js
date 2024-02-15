5828212014df108fd26f087fcd41ad5970975f06
usachin555 <sachinsac805@gmail.com>
2024-02-15 17:55:17 +0530

    this is 13th day task

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import Table from "./components/functional components/table"
 function App(){
   return(
     <div>
-<Table></Table>
-<Profile></Profile>
-<RulesOfJsx></RulesOfJsx>
-<Formss></Formss>
-<Pic></Pic>
-<Lisst></Lisst>
+<h1>hello world</h1>
     </div>
       )
 }
