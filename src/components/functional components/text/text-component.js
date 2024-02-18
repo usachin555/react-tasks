ed05e7736b200af28bc46bedc69075d62525afb1
usachin555 <sachinsac805@gmail.com>
2024-02-19 00:47:39 +0530

    parent, child, props added

---
+++ src/components/functional components/text/text-component.js
@@ -0,0 +1,10 @@
+const TextComponent =({text,textColor})=>{
+    // console.log(props)
+    return (
+        <div>
+            <h4 style={{color:textColor}}>{text}</h4>
+        </div>
+
+    )
+}
+export default TextComponent
